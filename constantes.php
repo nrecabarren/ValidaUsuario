b4aabd259145d2e154dd151db9cee9331e4e2c0d
Nicolas Recabarren <recabarren.valderrama.nicolas@gmail.com>
2017-06-19 20:49:08 -0400

    commit de prueba

--- constantes.php
+++ constantes.php
@@ -2,6 +2,7 @@
 
 define("PATH", $_SERVER["DOCUMENT_ROOT"].'/ValidaUsuario');
 define("URL", 'http://localhost:8081/ValidaUsuario/');
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
